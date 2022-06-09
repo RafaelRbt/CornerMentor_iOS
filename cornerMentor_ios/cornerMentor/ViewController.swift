e6863f12cb7ba5ebd0f90f30ab09384f15161772
RafaelRbt <46898196+RafaelRbt@users.noreply.github.com>
2022-06-08 22:55:20 -0500

    Carpeta del proyecto

---
+++ cornerMentor_ios/cornerMentor/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  cornerMentor
+//
+//  Created by MACBOOK PRO on 31/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
